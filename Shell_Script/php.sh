d1f0b4905693c511fd6bbac939b03045d3ffdd5f
Justine Dela Torre <97047918+Jasut1n@users.noreply.github.com>
2022-05-26 19:28:14 +0800

    ...

--- Shell_Script/php.sh
+++ Shell_Script/php.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+## Created by Jasutin
+
+checkroot() {
+    SAVE_LD_PRELOAD="$LD_PRELOAD"
+    unset LD_PRELOAD
+    if [ "$(id -u)" -ne 0 ]; then
+        printf "\e[1;31mPlease, run as root!\n\e[0m"
+        exit 1
+     fi
+     LD_PRELOAD="$SAVE_LD_PRELOAD"
+}
+
+checkroot
 
 clear;
 
